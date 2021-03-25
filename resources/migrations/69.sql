7066d12f6d199470160f6ef0b1aa5d4eff2be284
Petap0w <cks85@msn.com>
2021-03-25 16:07:08 +0100

    Fix Assignment Groups tables 2

--- resources/migrations/69.sql
+++ resources/migrations/69.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS `assignment_group` (
 );
 
 CREATE TABLE IF NOT EXISTS `assignment_group_assignment` (
-    assignment_group_name VARCHAR(30) NOT NULL,
-    assignment_id VARCHAR(5) NOT NULL,
-    PRIMARY KEY (assignment_group_name, assignment_id),
+    `assignment_group_name` VARCHAR(30) NOT NULL,
+    `assignment_id` int unsigned NOT NULL,
+    PRIMARY KEY (`assignment_group_name`, `assignment_id`),
     CONSTRAINT `assignment_group_assignment_fk_assignment_group_name` FOREIGN KEY (`assignment_group_name`) REFERENCES `assignment_group`(`name`) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT `assignment_group_assignment_fk_assignment_id` FOREIGN KEY (`assignment_id`) REFERENCES `assignment`(`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
