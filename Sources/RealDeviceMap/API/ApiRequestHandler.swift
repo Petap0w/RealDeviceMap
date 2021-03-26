20a75b9becfd455ef160e947e1de17858e94d988
Petap0w <cks85@msn.com>
2021-03-26 18:54:01 +0100

    Fix Device Group display 4

--- Sources/RealDeviceMap/API/ApiRequestHandler.swift
+++ Sources/RealDeviceMap/API/ApiRequestHandler.swift
@@ -1377,11 +1377,12 @@ class ApiRequestHandler {
                 for assignmentGroup in assignmentGroups! {
                     let assignmentsInGroup =
                         assignments?.filter({ assignmentGroup.assignmentIDs.contains($0.id!) }) ?? []
+                    let assignmentsInGroupDevice = assignmentsInGroup.filter({ $0.deviceUUID != nil })
+                    let assignmentsInGroupDeviceGroup = assignmentsInGroup.filter({ $0.deviceGroupName != nil})
+                    let assignmentsInGroupToShow = assignmentsInGroupDevice + assignmentsInGroupDeviceGroup
                     let assignmentsInGroupDevices = Array(
-                        Set(assignmentsInGroup.filter({ $0.deviceUUID != nil && $0.deviceGroupName == nil})
-                            .filter({ $0.deviceUUID == nil && $0.deviceGroupName != nil})
-                            .map({ $0.deviceUUID! + $0.deviceGroupName + " -> " + $0.instanceName}))
-                    )
+                        Set(assignmentsInGroupToShow.map({ $0.deviceUUID + $0.deviceGroupName + " -> " + $0.instanceName}))
+                    ).sorted()
 
                     var assignmentGroupData = [String: Any]()
                     assignmentGroupData["name"] = assignmentGroup.name
