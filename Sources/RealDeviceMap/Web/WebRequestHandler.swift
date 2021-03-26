1cc148e35058e751ed96a90e73e753c7de4479e5
Petap0w <cks85@msn.com>
2021-03-26 11:14:01 +0100

    Add Assignment Group functions 31

--- Sources/RealDeviceMap/Web/WebRequestHandler.swift
+++ Sources/RealDeviceMap/Web/WebRequestHandler.swift
@@ -3021,7 +3021,7 @@ class WebRequestHandler {
             data["error"] = "Invalid Request."
             return data
         }
-        let assignmentIDs = request.params(named: "assignments").map { UInt32($0) as! UInt32 }
+        let assignmentIDs = request.params(named: "assignments").map { UInt32($0)! }
         
 Log.info(message: "[DEBUG] assignmentIDs \(assignmentIDs)")
 Log.info(message: "[DEBUG] name \(groupName)")
