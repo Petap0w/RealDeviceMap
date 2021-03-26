d7271c400231d164de4993386cff67dcec503cf9
Petap0w <cks85@msn.com>
2021-03-26 11:34:16 +0100

    Add Assignment Group functions 36

--- Sources/RealDeviceMap/Web/WebRequestHandler.swift
+++ Sources/RealDeviceMap/Web/WebRequestHandler.swift
@@ -3008,7 +3008,7 @@ class WebRequestHandler {
             assignmentsData.append(["id": assignment.id ?? "" as Any, "deviceUUID": assignment.deviceUUID ?? "" as Any, "deviceGroupName": assignment.deviceGroupName ?? "" as Any, "instanceName": assignment.instanceName as Any, "selected": false])
         }
 
-        data["assignments"] = assignmentsData.sorted(by: { $0.deviceUUID < $1.deviceUUID })
+        data["assignments"] = assignmentsData.sorted { ($0.deviceUUID as? Any) < ($1.deviceUUID as? Any) }
 
         return data
     }
